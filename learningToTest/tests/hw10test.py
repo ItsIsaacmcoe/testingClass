7cbea955c6752beba46b0b2b6013ea5c1bf7eae5
Isaac <isaacmcoe73@gmail.com>
2023-10-03 17:46:44 -0400

    first commit

---
+++ learningToTest/tests/hw10test.py
@@ -0,0 +1,17 @@
+from learningToTest.projects.hw10.fruit_query import is_it_a_fruit
+
+
+def test_expected():
+    assert is_it_a_fruit('apple')
+
+
+def test_wrongdata_handling():
+    assert is_it_a_fruit(9)
+
+
+def test_case_handling():
+    assert is_it_a_fruit('pEar')
+
+
+def test_mispell_handling():
+    assert is_it_a_fruit('banana')
