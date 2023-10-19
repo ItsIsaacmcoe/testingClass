af5fae056a0657843d8163fd6b85d3c86e45cfd0
Isaac <isaacmcoe73@gmail.com>
2023-10-19 11:01:04 -0400

    homework 13
    use flake8 against all files and made appropriate changes

--- csc485/tests/hw11/test_hw11.py
+++ csc485/tests/hw11/test_hw11.py
@@ -6,7 +6,6 @@
 #
 # Does the program work when an integer is mixed into the string?
 
-import pytest
 from csc485.projects.hw11.get_formal_fruit import get_formal_name
 
 
--- csc485/tests/hw11/test_hw11_param.py
+++ csc485/tests/hw11/test_hw11_param.py
@@ -1,10 +1,16 @@
-#testing revised code
+# testing revised code
 from csc485.projects.hw11.formal_fruit_revised import get_formal_name
 import pytest
 
 
-@pytest.mark.parametrize('test_data,expected',
-                         [('apple', 'Malus domestica'), (3, 'fruit not on list'), ('cantaloupe', 'fruit not on list'),
-                          ('Apple', 'Malus domestica'), ('', 'fruit not on list')])
-def test_parametrized(test_data,expected):
+@pytest.mark.parametrize(
+    'test_data,expected', [
+        ('apple', 'Malus domestica'),
+        (3, 'fruit not on list'),
+        ('cantaloupe', 'fruit not on list'),
+        ('Apple', 'Malus domestica'),
+        ('', 'fruit not on list')
+    ]
+)
+def test_parametrized(test_data, expected):
     assert get_formal_name(test_data) == expected
