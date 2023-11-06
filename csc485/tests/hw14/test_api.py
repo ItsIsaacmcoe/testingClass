c2cad4e6d61e4032038f27e88a8ac08382ccc939
Isaac <isaacmcoe73@gmail.com>
2023-11-06 14:17:38 -0500

    hw14 content
    projects/hw14
    finalized
    
    tests/hw14
    finalized

--- csc485/tests/hw14/test_api.py
+++ csc485/tests/hw14/test_api.py
@@ -1,5 +1,16 @@
+"""
+testable things:
+    part 1:
+        is flask app imported properly
+    part 2:
+        Does api function when given special http characters? ie /
+        Does the function fail when given multiple arguments
+        Does the function work with a very long string
+"""
+
 import json
 import pytest
+import requests
 from csc485.projects.hw14.api import app  # this is the flask app
 
 
@@ -31,3 +42,23 @@ def test_api_error(client):
 
     with pytest.raises(ZeroDivisionError):
         response = client.get(f"/get_strength?password={password}")
+
+
+def test_app():
+    assert app.app_context is not None
+
+
+@pytest.mark.parametrize(
+    'test_data,expected', [
+        ('http://127.0.0.1:5000/get_strength?password=/%%/%%%%', 200),
+        ('http://127.0.0.1:5000/get_strength?password=pass1?password=pass2', 200),
+        ('http://127.0.0.1:5000/get_strength?password=asfasdfasdffasdfas'
+         'dfassdfassdfasdfasdfasdfasdfasdf'
+         'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfassfasdfasf'
+         'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf'
+         'asdfasdfasfasdfasdfasdfasdfasdfassdfasdfasdfasdfasdf', 200)
+    ]
+)
+def test_running(test_data, expected):
+    r = requests.get(test_data)
+    assert r.status_code == expected
